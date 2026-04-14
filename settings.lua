6cab4a7a66c243d4f03b70cd09599943d39156d5
zach863 <zach@txjackson.com>
2026-04-14 11:58:29 -0500

    Fix train stop circuit network lookup crash on init

---
+++ settings.lua
@@ -0,0 +1,11 @@
+data:extend({
+  {
+    type = "int-setting",
+    name = "simplify-train-refresh-ticks",
+    setting_type = "runtime-global",
+    default_value = 300,
+    minimum_value = 60,
+    maximum_value = 3600,
+    order = "a"
+  }
+})
